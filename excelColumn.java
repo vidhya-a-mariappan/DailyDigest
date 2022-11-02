b39cbde59efe7984ceab68d52c85add7567c60ac
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-11-02 22:21:46 +0530

    Create excelColumn.java

---
+++ excelColumn.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String convertToTitle(int columnNumber) {
+       String result="";
+       while(columnNumber > 0) {
+           columnNumber--;
+           result = (char)('A' + columnNumber%26) + result;
+           columnNumber/=26;
+       }
+       return result;
+    }
+}
