d12ab9e1b8264884c99f6363c68b463b2e03bd20
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-11-03 15:05:33 +0530

    Create islandPerimeter.java

---
+++ islandPerimeter.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+         int land=0,adjacent=0;
+        for (int i=0;i<grid.length;i++) {
+            for (int j=0;j<grid[i].length;j++) {
+                if (grid[i][j]==1){
+                    land++;
+                    if(j<grid[i].length-1 && grid[i][j+1]==1)
+                        adjacent+=2;
+                     if(i<grid.length-1 && grid[i+1][j]==1)
+                        adjacent+=2;
+                 }
+            }
+        }
+        int perimeter=land*4-adjacent;
+        return  perimeter;
+    }
+}
