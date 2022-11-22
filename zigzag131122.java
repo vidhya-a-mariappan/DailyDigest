49e91df0d3c0b56057701eddc00c54c1e05c86a2
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-11-22 09:39:38 +0530

    Create zigzag131122.java

---
+++ zigzag131122.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        StringBuilder[] sbs = new StringBuilder[numRows];
+        int row=0, dir = 0;
+        for (int i =0;i<sbs.length; i++)
+            sbs[i]= new StringBuilder();
+        for (int i = 0; i<s.length(); i++){
+            char c=s.charAt(i);
+            row += dir;
+            sbs[row].append(c);
+            if (row == 0 || row == numRows - 1)
+                dir =(dir == 0)?1: -dir;
+        }
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb: sbs)
+            result.append(sb.toString());
+        return result.toString();                                      
+  }
+}
